af056544505941bc2f2674c27f5f3b0aa3644915
Aditya Ravichandran <adityaravichandran6@gmail.com>
2025-03-26 00:29:46 +0530

    Add markdown plugin

--- init.lua
+++ init.lua
@@ -908,7 +908,6 @@ require('lazy').setup({
     config = function()
       ---@diagnostic disable-next-line: missing-fields
       require('tokyonight').setup {
-        transparent = true,
         styles = {
           comments = { italic = false }, -- Disable italics in comments
         },
